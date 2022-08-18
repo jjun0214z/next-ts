6c1567232651446d1a56c4fa352a7d5bef482a54
박준민 <jjun0214@gmail.com>
2022-08-18 13:57:47 +0900

    enzyme import 에러 수정

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { mount, shallow } from 'enzyme';
 import IndexPage from '@/pages/index';
 
 describe('With Enzyme', () => {
--- enzyme.ts
+++
@@ -1,3 +0,0 @@
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-require('enzyme').configure({ adapter: new Adapter() });
