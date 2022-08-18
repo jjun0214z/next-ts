62a211becdd7517cd4afb092cf3ef6b5d57c6f3d
박준민 <jjun0214@gmail.com>
2022-08-18 11:03:06 +0900

    styled-component 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from 'next/app';
 import wrapper from '@/store/configureStore';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@/assets/styles/theme';
+import GlobalStyle from '@/assets/styles/globalStyles';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
